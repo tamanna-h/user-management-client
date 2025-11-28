44223ccb6cdb1fc79b06ab24bb1f22953727a303
Tamanna <tahminadox@gmail.com>
2025-11-28 15:21:49 +0600

    Use fetch to load data, enable CORS middleware in server

---
+++ src/components/Users.jsx
@@ -0,0 +1,14 @@
+import React, { use } from 'react';
+
+const Users = ({usersPromise}) => {
+    const users = use(usersPromise);
+    console.log(users);
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
