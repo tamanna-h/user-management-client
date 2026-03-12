ab46b1cc7425984e5c44f8b07eb5bf025a93736b
Tamanna <tahminadox@gmail.com>
2026-03-12 12:48:14 +0600

    feat: add user form and users list UI

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,10 +3,29 @@ import React, { use } from 'react';
 const Users = ({usersPromise}) => {
     const users = use(usersPromise);
     console.log(users);
+
+    const handleAddUser = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const user = {name, email};
+        console.log(user);
+    }
     
     return (
         <div>
-            
+            <form onSubmit={handleAddUser}>
+                <input name='name' type="text" />
+                <br />
+                <input type="email" name='email' />
+                <br />
+                <input type="submit" value="Add User"/>
+            </form>
+
+            <div>
+                {users.map(user => <div key={user.id}>{user.name} : {user.email}</div>)}
+            </div> 
         </div>
     );
 };
