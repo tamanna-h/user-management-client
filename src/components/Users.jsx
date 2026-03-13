5ae48b89eca5a1e4d7a4c3511d76f678a0f4936e
Tamanna <tahminadox@gmail.com>
2026-03-13 11:56:43 +0600

    feat(users): send user data to server using fetch POST request

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -11,6 +11,20 @@ const Users = ({usersPromise}) => {
         const email = form.email.value;
         const user = {name, email};
         console.log(user);
+
+        // create user in the server
+        fetch('http://localhost:3000/users', {
+             method: 'POST',
+             headers: {
+                'content-type': 'application/json'
+             },
+             body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('data after post', data);
+        })
+
     }
     
     return (
